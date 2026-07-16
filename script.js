b73b1798604f901f83db005986bfa80e344c84a3
Felipe Burmann <117690298+Lipeburmann@users.noreply.github.com>
2026-07-16 10:41:54 -0300

    Enhance greeting button functionality with title update
    Added title element update and image styling on button click.

--- script.js
+++ script.js
@@ -2,14 +2,17 @@ const botao = document.getElementById('btnCumprimentar');
 const imagemGif = document.getElementById('tobiGif');
 const mensagem = document.getElementById('mensagem');
 const musica = document.getElementById('musicaFundo');
-
-
+const titulo = document.getElementById('titulo');
 const gifNormal = 'assets/tobi_inicial.gif';
 const gifCumprimento = 'assets/mao_loop_count.gif';
+
 musica.volume = 0.1;
 
 
 botao.addEventListener('click', function() {
         musica.play();
         imagemGif.src = gifCumprimento;
+        titulo.textContent = 'AU AU!
+        imagemGif.style.maxWidth = '320px';
+        imagemGif.style.height = '320px';        
 });
