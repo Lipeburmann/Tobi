d3632d13ff65874fad15fb2a90d0099efb6bd861
Felipe Burmann <117690298+Lipeburmann@users.noreply.github.com>
2026-07-21 10:18:29 -0300

    Atualizar o script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const titulo = document.getElementById('titulo');
 const musicaFundo = document.getElementById('musicaFundo');
 const somEfeito = document.getElementById('somEfeito');
 const gifCumprimento = 'assets/mao_loop_count.gif';
-musicaFundo.volume = 0.5;
+musicaFundo.volume = 0.35;
 somEfeito.volume = 1;
 let timeoutComida = null;
 
